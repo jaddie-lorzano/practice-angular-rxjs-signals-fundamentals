7818a0db72247596bff0003efa1fbfbd1d119bd3
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-28 03:10:18 +0800

    setup cart item component

--- src/app/cart/cart-item/cart-item.component.ts
+++ src/app/cart/cart-item/cart-item.component.ts
@@ -1,12 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { CartItem } from '../cart';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'pm-cart-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cart-item.component.html',
 })
 export class CartItemComponent {
   @Input({ required: true }) cartItem!: CartItem;
+
+  // Quantity available (hard-coded to 8)
+  // Mapped to an array from 1 - 8
+  quantityArray = [...Array(8).keys()].map((x) => x + 1);
+
+  // Calculate the extended price
+  extendedPrice = this.cartItem?.quantity * this.cartItem?.product.price;
+
+  onQuantitySelected(quantity: number): void {}
+  removeFromCart(): void {}
 }
