8bf260360aa104ff4925d89f9556b4d381109731
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-28 03:12:55 +0800

    setup cart shell component

--- src/app/cart/cart-shell/cart-shell.component.ts
+++ src/app/cart/cart-shell/cart-shell.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CartListComponent } from '../cart-list/cart-list.component';
+import { CartTotalComponent } from '../cart-total/cart-total.component';
 
 @Component({
   selector: 'pm-cart-shell',
   standalone: true,
-  imports: [],
   templateUrl: './cart-shell.component.html',
+  imports: [CartListComponent, CartTotalComponent],
 })
 export class CartShellComponent {}
