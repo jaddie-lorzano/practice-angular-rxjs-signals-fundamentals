d5778ce07b1c818f8976c136cf08d0cd74445312
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-29 16:36:47 +0800

    get products from product service

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
@@ -9,13 +11,27 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
   templateUrl: './product-list.component.html',
   imports: [CommonModule, ProductDetailComponent],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
 
   // Products
   products: Product[] = [];
+
+  private productService = inject(ProductService);
+  private sub!: Subscription;
+
+  ngOnInit(): void {
+    this.sub = this.productService
+      .getProducts()
+      .pipe(tap(() => console.log('In component pipeline')))
+      .subscribe((products) => (this.products = products));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
