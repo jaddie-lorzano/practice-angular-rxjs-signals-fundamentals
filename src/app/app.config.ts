bd3bfdf247670ea8497848fdf6b833b4effb7fc3
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-29 16:34:18 +0800

    add http client module in the app provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,12 @@ import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app.data';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
+      HttpClientModule,
       FormsModule,
       InMemoryWebApiModule.forRoot(AppData, { delay: 1000 })
     ),
