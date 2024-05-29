80c2c36c8abcb62c053ce79605a88f67c49d19e2
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-29 16:36:02 +0800

    add get products in product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -6,4 +9,13 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   // Just enough here for the code to compile
   private productUrl = 'api/products';
+
+  private http = inject(HttpClient);
+  // constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(this.productUrl)
+      .pipe(tap((data) => console.table(data)));
+  }
 }
